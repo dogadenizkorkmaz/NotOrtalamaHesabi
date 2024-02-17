aacfad7ee342b16da269c4efa665f7d0e524f48a
Doga <dogadenizkorkmaz@gmail.com>
2024-02-17 22:24:26 +0300

    boolean geçti-kaldı kısmı x-!x olarak değiştirilirdi. ilgili bölüme açıklama eklendi

--- src/NotOrtalamaHesabı.java
+++ src/NotOrtalamaHesabı.java
@@ -24,15 +24,18 @@ public class NotOrtalamaHesabı {
 
         System.out.println("Müzik Notunuz : ");
         muzik = inp.nextInt();
-
+        //Toplam puan, ders sayısına bölünür.
         int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
         double sonuc = toplam / 6.0;
         System.out.println("Ortalamanız : " + sonuc);
+        //Sonuç olarak ortalama elde edilir.
 
-        double a = sonuc, b = 60;
-        boolean x = a > b;
-        boolean y = a < b;
-        boolean kaldi = !x && y;
+        //Ortalama 60'tan büyük ise sınıfı geçti anlamına gelir.
+        double a=sonuc, b=60;
+        boolean x = a>=b;
+        // boolean y = a < b;
+        boolean kaldi = !x; // && y;
+        boolean gecti = x;
         String kalanortalama = kaldi ? "Sınıfta Kaldı" : "Sınıfı Geçti";
         System.out.println(kalanortalama);
     }
