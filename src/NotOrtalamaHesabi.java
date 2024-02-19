93537e0955c88eb2bb285140568cefa998bda75e
Doga <dogadenizkorkmaz@gmail.com>
2024-02-19 16:32:27 +0300

    class name changed

--- src/NotOrtalamaHesabi.java
+++ src/NotOrtalamaHesabi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NotOrtalamaHesabı {
+public class NotOrtalamaHesabi {
     public static void main(String[] args) {
         //ders isimleri tanımla
         int mat, fizik, kimya, turkce, tarih, muzik;
